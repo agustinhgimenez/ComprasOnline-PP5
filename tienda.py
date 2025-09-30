3886b4da97c37d3077d479302c6b52c52b9ee22b
Agustin Gimenez <agimenez@fie.undef.edu.ar>
2025-09-30 11:13:58 +0000

    Tienda.py inicial

--- tienda.py
+++ tienda.py
@@ -1,3 +1,15 @@
 class Tienda:
     def __init__(self):
         pass
+
+    def agregar_usuario(self, usuario):
+        pass
+
+    def agregar_producto(self, producto):
+        pass
+
+    def gestionar_venta(self, usuario):
+        pass
+
+    def gestionar_morosidad(self):
+        pass
