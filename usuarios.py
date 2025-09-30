8c9a2e3654107e263870bc419b6b284eb8da09e7
Agustin Gimenez <agimenez@fie.undef.edu.ar>
2025-09-30 11:11:40 +0000

    Usuarios Estructura Inicial

--- usuarios.py
+++ usuarios.py
@@ -1,4 +1,26 @@
 class Usuario:
-    def __init__(self):
+    def __init__(self, nombre, edad, saldo, puntos, es_extranjero):
+        self.nombre = nombre
+        self.edad = edad
+        self.saldo = saldo
+        self.puntos = puntos
+        self.es_extranjero = es_extranjero
+
+
+    def agregar_al_carrito(self, producto):
+        pass
+    
+    def cargar_saldo(self, monto):
         pass
 
+    def realizar_compra(self):
+        pass
+
+    def actualizar_nivel(self):
+        pass
+
+    def calcular_nivel(self):
+        pass
+
+
+  
